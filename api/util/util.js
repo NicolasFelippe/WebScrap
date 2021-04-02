4d4a80e6483fb5aa8d8939bd7057be57a517bcea
Junior Avelino <junior.a98@hotmail.com>
2021-04-02 18:40:22 -0300

    adicionado log do tempo a ser executado o interval

--- api/util/util.js
+++ api/util/util.js
@@ -1,5 +1,7 @@
 const getRandomNumber = (min, max) => {
-    return (Math.floor(Math.random() * (max - min + 1)) + min) * 1000;
+    const time = (Math.floor(Math.random() * (max - min + 1)) + min) * 1000;
+    console.log(`tempo para da próxima chamada: ${time.toString().substring(0, 2)} segundos`);
+    return time;
 }
 
 module.exports = {
