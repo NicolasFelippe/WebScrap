d38e2d9d3fb087ddaffcbccd4bc4c84e2536cf55
Nicolas Felippe <nicolas.rocha@agapys.com>
2021-05-23 23:19:24 -0300

    robo pai e filhos

--- bin/WebAutomation.js
+++ bin/WebAutomation.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv').config();
 const WebScrapingService = require('../api/services/WebScraping');
-const TelegramBot = require(`node-telegram-bot-api`);
 const { login } = require('../api/services/eurobets-service')
 const { getRandomNumber, logger, JsonToString } = require('../api/util/utils');
 const delay = require('delay');
@@ -16,18 +15,7 @@ class Main {
         if (dotenv.error) {
             console.debug(error);
         }
-        const { USER, PASS, TOKEN_TELEGRAM, GROUP_ID_TELEGRAM, MULTIPLYBET, COOKIE, GROUP_NOTIFICATION } = dotenv.parsed;
-
-        let telegramService = new TelegramBot(TOKEN_TELEGRAM, { polling: true });
-
-        // FUNÇÃO USADA PARA DESCOBRIR O ID DO GRUPO TELEGRAM
-        // telegramService.on('message', (msg) => {
-        //     const chatId = msg.chat.id;
-        //     console.log(chatId)
-        // send a message to the chat acknowledging receipt of their message
-        // telegramService.sendMessage(chatId, 'Received your message');
-        // });
-
+        const { USER, PASS, MULTIPLYBET, COOKIE } = dotenv.parsed;
 
         let validatedBets = null;
         let auth = false
@@ -53,42 +41,15 @@ class Main {
             if (Array.isArray(myBetsOpen) && myBetsOpen.length > 0) validatedBets = webScraping.validateBets(myBetsOpen);
             let newBets = null
             if (Array.isArray(validatedBets) && validatedBets.length > 0) {
-                console.log('teste', validatedBets)
                 newBets = await webScraping.verifyNewBets(validatedBets, this.#bets);
-
                 this.#bets.push(...validatedBets);
-
             }
 
             if (Array.isArray(newBets) && newBets.length > 0) {
-                if (telegramService) {
-                    const msg = JsonToString(newBets.map(bet => ({
-                        'Partida': bet.match,
-                        'Time Casa': bet.timeCasa,
-                        'Time Visitante': bet.timeVisitante,
-                        'Data Jogo': `${bet.dataJogo} - ${bet.horaJogo}`,
-                        'Aposta': `${bet.statusAposta} - ${bet.time}`,
-                        'Odd': bet.odd
-                    })))
-                    telegramService.sendMessage(GROUP_ID_TELEGRAM, `Novas bets encontradas:\n${msg}`)
-                        .then((success) => console.log('mensagem enviada ao grupo'))
-                        .catch((err) => console.log('erro ao enviar mensagem para o grupo', err));
-                    telegramService.sendMessage(GROUP_NOTIFICATION, `NOVAS ENTRADAS DO BOT:\n${msg}`)
-                        .then((success) => logger('mensagem enviada ao grupo de notificação'))
-                        .catch((err) => logger('erro ao enviar mensagem para o grupo notificação', JsonToString(err)));
-                }
-
-                const response = await webScraping.validationGames(newBets, MULTIPLYBET);
-                if (telegramService) {
-                    telegramService.sendMessage(GROUP_ID_TELEGRAM, `SUCESSO REPLICADAS \n ${JsonToString(response)}`)
-                        .then((success) => logger('mensagem enviada ao grupo'))
-                        .catch((err) => logger('erro ao enviar mensagem para o grupo', JsonToString(err)));
-                }
-
+               await webScraping.validationGames(newBets, MULTIPLYBET);
             }
 
             let time = getRandomNumber(1, 3);
-            console.log('await delay ', time)
             await delay(time);
         }
     }
