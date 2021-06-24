8bb368aee2e4a6bcccb835ed7a5eed3f175fa810
Nicolas Felippe <nicolas.rocha@agapys.com>
2021-06-24 00:06:11 -0300

    multiplica porcetagem

--- api/services/reply-bets.service.js
+++ api/services/reply-bets.service.js
@@ -4,7 +4,7 @@ const { logger } = require('../util/utils')
 const replyBets = async (users, idMatch, betId, multiplyChild) => {
     for (user of users) {
         logger('[INIT] [Reply Bets] replayBets()', `execucao cmd: ${user.login}, ${idMatch}, ${betId}, ${multiplyChild}, ${user.stake}`)
-        cmd.execute(`./processChildren.sh`, user.login, user.password, user.stake * multiplyChild, idMatch, betId, user.stakeType, user.percent, '&')
+        cmd.execute(`./processChildren.sh`, user.login, user.password, user.stake * multiplyChild, idMatch, betId, user.stakeType, user.percent * multiplyChild, '&')
     }
 }
 
