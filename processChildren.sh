7cf9150ff544edc04f0b42d98c1b47addc73e2df
Junior Avelino <junior.a98@hotmail.com>
2021-06-23 23:12:59 -0300

    novas opções adicionada para envio ao robô filho

--- processChildren.sh
+++ processChildren.sh
@@ -4,4 +4,6 @@ echo "user: $1"
 echo "valoraposta: $3"
 echo "idMatch: $4"
 echo "betId: $5"
-npm run children user=$1 password=$2 valorAposta=$3 idMatch=$4 idBet=$5
+echo "stakeType: $6"
+echo "percent: $7"
+npm run children user=$1 password=$2 valorAposta=$3 idMatch=$4 idBet=$5 stakeType=$6 percent=$7
